1efc5f6f2d5b20d9a80b1f982ee11f8f5b23f974
oyelakinG9 <oyelakinr37@gmail.com>
2022-08-25 23:49:26 +0100

    Modify rental ruby file

--- rental.rb
+++ rental.rb
@@ -16,3 +16,13 @@ end
 
 friend = Person.new(12, 'Carlos')
 book1 = Book.new('565', 'Andres')
+
+book1.add_rental('25-08-2022', book1)
+p book1.rentals.first
+p book1.rentals.first.person.name
+
+friend.add_rental('01-01-2022', book2)
+
+p friend.rentals
+p friend.rentals.first.book.tittle
+p friend.rentals.length
