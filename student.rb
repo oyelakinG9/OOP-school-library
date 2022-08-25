a67df0c23cd03e36baa8d4c61920048b5374f365
oyelakinG9 <oyelakinr37@gmail.com>
2022-08-25 23:37:16 +0100

    Create student ruby file

--- student.rb
+++ student.rb
@@ -1,4 +1,5 @@
 require './person'
+require './classroom'
 
 # child class inheritance from person
 class Student < Person
@@ -9,6 +10,11 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
 
