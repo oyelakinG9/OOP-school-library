eaa6292f4744709625634cfed8a774550db36f83
oyelakinG9 <oyelakinr37@gmail.com>
2022-08-26 22:47:24 +0100

    Fix linters error

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 require './app'
 
 def main
-  puts ''
   puts 'Please choose an option by enterin a number:'
-  options = ['List all books', 'List all people', 'Create a person', 'Create a book', 'Create a rental', 'List all rentals for a given person id', 'Exit']
+  options = ['List all books', 'List all people', 'Create a person', 'Create a book', 'Create a rental',
+    'List all rentals for a given person id', 'Exit']
   options.each_with_index { |v, k| puts "#{k + 1} - #{v}" }
 
   selected = gets.chomp
